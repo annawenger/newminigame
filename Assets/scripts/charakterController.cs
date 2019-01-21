8da1b794bc32a1e2e5181ab2a6556ced903d7b5c
annawenger <45820274+annawenger@users.noreply.github.com>
2019-01-21 20:50:24 +0100

    add rawPosition

---
+++ Assets/scripts/charakterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charakterController : MonoBehaviour {
+
+    public Camera cam;
+
+
+	// Use this for initialization
+	void Start () {
+		if (camera == null)
+        {
+            cam = Camera.main
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+}
---
+++ Assets/scripts/randomgenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class randomgenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        System.Random rnd = new System.Random(123);
+        Debug.Log(rnd.Next(1,5));
+        Debug.Log(rnd.Next(1,5));
+        Debug.Log(rnd.Next(1,5));
+        Debug.Log(UnityEngine.Random.Range(1,5));
+        Debug.Log(RandomValueint());
+    }
+
+    public void RandomValue(){
+            System.Random rnd = new System.Random(123);  //deklaration einer Methode
+            Debug.Log(rnd.Next(1,5));
+        }
+    public int RandomValueint()
+    {
+        System.Random rnd = new System.Random(123);  //deklaration einer Methode
+        return rnd.Next(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
