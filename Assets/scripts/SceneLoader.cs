2c2e0bb62155e107004e549d33d5f47c1f5e19fd
smeerws <hello@smeerws.at>
2019-01-21 18:28:34 +0100

    Renamed Folders, Scripts, Scenes CamelCase Added Prefabs Folder (added SceneLoader Prefab -- Empty GameObject with SceneLoader Script) fixed Canvas/Button visibility in Start scene (del old canvas, new canvas, reset position) fixed blurry text on button (increase font size, set vertical/horizontal overflow to overflow) resize (scale down) text Object added functionality to button (press button start, call sceneLoader.loadMainGame() methode.

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    enum Scenes { Start, Main, GameOver, Won };
+
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Start);
+    }
+
+    public void LoadMainGameScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Main);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene((int)Scenes.GameOver);
+    }
+
+    public void LoadWonScene()
+    {
+        SceneManager.LoadScene((int)Scenes.GameOver);
+    }
+}
