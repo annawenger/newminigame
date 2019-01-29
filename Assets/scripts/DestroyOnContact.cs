d84520ee07d33a88a4454e99c606cd287483a554
annawenger <45820274+annawenger@users.noreply.github.com>
2019-01-29 09:02:28 +0100

    created DestroyBall

--- Assets/scripts/DestroyOnContact.cs
+++ Assets/scripts/DestroyOnContact.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOnContact : MonoBehaviour {
+public class DestroyBall : MonoBehaviour {
 
 void OnTriggerEnter2D (Collider2D other)
     {
