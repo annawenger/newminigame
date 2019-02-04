4308f176626cbf4d23a480dd31e15d22204e2dea
annawenger <45820274+annawenger@users.noreply.github.com>
2019-02-04 20:26:16 +0100

    added BallSpawner,is working but spawned object aren't visible

---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject ball;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-8.5f, 8.5f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(ball, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+
