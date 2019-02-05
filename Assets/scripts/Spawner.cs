ea08391e9c6018a3413ca1b925d16b19c4d78e29
annawenger <45820274+annawenger@users.noreply.github.com>
2019-02-05 08:19:42 +0100

    spawned balls are visible,but balls are to fast spawned

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -9,14 +9,14 @@ public class Spawner : MonoBehaviour
     public GameObject ball;
     float randX;
     Vector2 whereToSpawn;
-    public float spawnRate = 2f;
+    public float spawnRate = 10f;
     float nextSpawn = 0.0f;
 
     void Update()
     {
         if (Time.time > nextSpawn)
             nextSpawn = Time.time + spawnRate;
-            randX = Random.Range(-8.5f, 8.5f);
+            randX = Random.Range(-8f, 8f);
             whereToSpawn = new Vector2(randX, transform.position.y);
             Instantiate(ball, whereToSpawn, Quaternion.identity);
         }
